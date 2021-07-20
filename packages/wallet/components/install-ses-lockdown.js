cb83e04ac98ad52ab8c5fc4d825d3b57df5a2450
Michael FIG <mfig@agoric.com>
2021-07-19 21:28:38 -0600

    feat(wallet): new bare-bones keplr.html

--- packages/wallet/components/install-ses-lockdown.js
+++ packages/wallet/components/install-ses-lockdown.js
@@ -2,7 +2,11 @@
 import 'ses';
 import '@agoric/eventual-send/shim';
 
-lockdown({ errorTaming: 'unsafe', stackFiltering: 'verbose' });
+lockdown({
+  errorTaming: 'unsafe',
+  stackFiltering: 'verbose',
+  overrideTaming: 'severe',
+});
 
 // Even on non-v8, we tame the start compartment's Error constructor so
 // this assignment is not rejected, even if it does nothing.
